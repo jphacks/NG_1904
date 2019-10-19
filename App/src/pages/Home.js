43220fab255009eeff78213a9e10455926d84af9
maitaken <150441004@ccmailg.meijo-u.ac.jp>
2019-10-19 17:22:54 +0900

    Fixed 録音し続ける動作のバグ修正

--- App/src/pages/Home.js
+++ App/src/pages/Home.js
@@ -2,46 +2,48 @@ import React,{ useState } from 'react';
 import './Home.css';
 import { useHistory } from 'react-router-dom';
 
-import { morphologicalAnalysis } from '../assets/util'
+import { morphologicalAnalysis,vibrate } from '../assets/util'
 
 export default function Home() {
-    let [ recognition,setRecognition ] = useState(null);
+    let [ isRecording,setIsRecording ] = useState(false);
     let [ targetMuzzle,setTargetMuzzle ] = useState({
         'text':'えっ',
     });
-  
-  const history = useHistory();
+
+    let recognition;
+    const history = useHistory();
 
     function recordStart() {
         window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-        let recognition = new window.SpeechRecognition();
+        recognition = new window.SpeechRecognition();
         recognition.continuous = true;
         recognition.lang = "ja-JP";
-        setRecognition(recognition)
+        setIsRecording(true)
     
         recognition.onerror = function(event){
             window.alert(event.error);
         }
-    
-        recognition.onresult = async function(event){
+
+        recognition.onresult = async function(event)  {
             let text = event.results[event.results.length-1][0].transcript;
+            await vibrate();
             let tokens = await morphologicalAnalysis(text);
             for(let token of tokens) {
                 if(token['surface_form'] == targetMuzzle['text']) {
-                    // ここにバイブレーションの動作を追加
+                    vibrate();
                 }
             }
-
         }
         recognition.start();
     }
 
     function recordStop() {
-        recognition.stop();
-        setRecognition(null);
+        recognition.abort();
+        recognition = null;
+        setIsRecording(false);
     }
     
-    let recordButton = ( recognition )? (
+    let recordButton = ( isRecording )? (
         <button onClick={recordStop}>RecordStop</button>
     ):(
         <button onClick={recordStart}>RecordStart</button>
