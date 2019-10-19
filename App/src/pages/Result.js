66c63684365a1e3175f535e1cf4b76bdabdfa3ea
asukaokochi <x17018xx@gmail.com>
2019-10-20 04:08:15 +0900

    rankingをいい感じにしてる

--- App/src/pages/Result.js
+++ App/src/pages/Result.js
@@ -4,28 +4,36 @@ import { useHistory } from 'react-router-dom';
 import testData from './test.json';
 import LEFT from '../assets/img/left-accessory.png';
 import RIGHT from '../assets/img/right-accessory.png';
+import ONE from '../assets/img/1.png';
+import TWO from '../assets/img/2.png';
+import THREE from '../assets/img/3.png';
+import FOUR from '../assets/img/4.png';
+import FIVE from '../assets/img/5.png';
 
 export default function Result() {
     const history = useHistory();
 
+    const images = [ONE, TWO, THREE, FOUR, FIVE];
+
+
     const listItems = testData.map((data, index) =>
             <li key={index} className="List-item" onClick={()=>history.push('/home')}>
-                {data.str}{" ： "}{data.count}<span>{"回"}</span>
+            <img src={images[index]}></img>
+                { data.str }{ " ： "}{ data.count } < span > { "回"}</span >
                 {/* <button >LOG</button> */}
             </li>
     );
 
     return (
         <div className="App-body">
+            <div className="background-circle"></div>
             <header className="App-body_rank-head">
                 <img src={LEFT} alt="左の装飾"/>
                 <h1 className="App-body_rank-header">あなたの口グセ<br></br><span className="App-body_rank-header-top">TOP5</span></h1>
                 <img src={RIGHT} alt="右の装飾"/>
             </header>
-            <div>
-                <div>
+            <div className="App-body_rank-list">
                     <ol className="List">{listItems}</ol>
-                </div>
             </div>
         </div>
     );
