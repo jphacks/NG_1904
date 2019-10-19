24b2439e7068c0403665d2d151acf144ddebb802
maitaken <150441004@ccmailg.meijo-u.ac.jp>
2019-10-19 17:22:54 +0900

    マージ

--- App/src/pages/Home.js
+++ App/src/pages/Home.js
@@ -1,36 +1,39 @@
 import React,{ useState } from 'react';
 import './Home.css';
 import { useHistory, useLocation } from 'react-router-dom';
-import { morphologicalAnalysis } from '../assets/util'
+
+import { morphologicalAnalysis,vibrate } from '../assets/util'
 
 export default function Home() {
-    let [ recognition,setRecognition ] = useState(null);
+    let [ isRecording,setIsRecording ] = useState(false);
     let [ targetMuzzle,setTargetMuzzle ] = useState({
         'text':'えっ',
     });
     const [data, setData] = useState([]);
 
-    const location = useLocation();
+    let recognition;
+    const location = useLocation();    
     const history = useHistory();
-    
 
     function recordStart() {
         window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-        let recognition = new window.SpeechRecognition();
+        recognition = new window.SpeechRecognition();
         recognition.continuous = true;
         recognition.lang = "ja-JP";
-        setRecognition(recognition)
+        setIsRecording(true)
     
         recognition.onerror = function(event){
             window.alert(event.error);
         }
-    
-        recognition.onresult = async function (event) {
+
+        recognition.onresult = async function(event)  {
             let text = event.results[event.results.length-1][0].transcript;
+            await vibrate();
             let tokens = await morphologicalAnalysis(text);
             for(let token of tokens) {
                 if(token['surface_form'] === targetMuzzle['text']) {
                     // ここにバイブレーションの動作を追加
+                    vibrate()
                 }
                 let data_copy =  Object.create(data);
                 data_copy.push(token['surface_form']);
@@ -45,8 +48,9 @@ export default function Home() {
     }
 
     function recordStop() {
-        recognition.stop();
-        setRecognition(null);
+        recognition.abort();
+        recognition = null;
+        setIsRecording(false);
     }
 
     function test () {
@@ -61,7 +65,7 @@ export default function Home() {
         <a>えっとを治そう</a>
     )
     
-    let recordButton = ( recognition )? (
+    let recordButton = ( isRecording )? (
         <button onClick={recordStop}>RecordStop</button>
     ):(
         <button onClick={recordStart}>RecordStart</button>
