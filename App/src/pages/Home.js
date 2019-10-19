6f9536605a5673de5de8ec8390421efcef33b537
maitaken <150441004@ccmailg.meijo-u.ac.jp>
2019-10-19 18:30:03 +0900

    Fixed Abortした時にエラーが出る現象を直しました．

--- App/src/pages/Home.js
+++ App/src/pages/Home.js
@@ -6,40 +6,45 @@ import { morphologicalAnalysis,vibrate } from '../assets/util'
 
 export default function Home() {
     let [ isRecording,setIsRecording ] = useState(false);
+    let [ recognition,setRecognition ] = useState(null)
     let [ targetMuzzle,setTargetMuzzle ] = useState({
         'text':'えっ',
     });
+    let [ wordList,setWordList ] = useState({
+        words:[]
+    })
 
-    let recognition;
     const history = useHistory();
 
     function recordStart() {
         window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-        recognition = new window.SpeechRecognition();
+        let recognition = new window.SpeechRecognition();
         recognition.continuous = true;
         recognition.lang = "ja-JP";
         setIsRecording(true)
     
-        recognition.onerror = function(event){
-            window.alert(event.error);
-        }
+        // recognition.onerror = function(event){
+        // }
 
         recognition.onresult = async function(event)  {
             let text = event.results[event.results.length-1][0].transcript;
             await vibrate();
             let tokens = await morphologicalAnalysis(text);
             for(let token of tokens) {
+                console.log(token)
+                wordList['words'].push(token['surface_form'])
                 if(token['surface_form'] == targetMuzzle['text']) {
                     vibrate();
                 }
             }
+
         }
         recognition.start();
+        setRecognition(recognition);
     }
 
     function recordStop() {
         recognition.abort();
-        recognition = null;
         setIsRecording(false);
     }
     
