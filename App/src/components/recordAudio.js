ebfb460c8624f32bb5263765d6e3bc33725b17c7
jinyagi <jinjin.0327.jinjin@gmail.com>
2019-10-19 10:30:31 +0900

    ADD：録音するコンポーネント

---
+++ App/src/components/recordAudio.js
@@ -0,0 +1,19 @@
+//録音するコンポーネント
+
+export default function recordAudio () {
+
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    var recognition = new window.SpeechRecognition();
+
+    recognition.onerror = function(event){
+        window.alert(event.error);
+    }
+
+    recognition.onresult = function(event){
+        if(0 < event.results.length){
+            window.alert( vent.results[0][0].transcript);
+        }
+    }
+    
+    recognition.start();
+}
