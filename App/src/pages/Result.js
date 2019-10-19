cd1d3523129caa7d5409ae8e48ce22ba97228ad0
jinyagi <jinjin.0327.jinjin@gmail.com>
2019-10-19 13:43:03 +0900

    簡素なLISTの実装

--- App/src/pages/Result.js
+++ App/src/pages/Result.js
@@ -4,18 +4,26 @@ import { Link } from 'react-router-dom';
 import testData from './test.json';
 
 export default function Result() {
-    
+
+    const listItems = testData.map((data, index) =>
+        <Link to="/Home">
+            <li key={index} className="List-item">
+                {data.str}{" ： "}{data.count}
+                <button >LOG</button>
+            </li>
+        </Link>
+    );
+
     return (
-        <div className="App">
+        <>
             <header>
-                <h1>ResultPage</h1>
+                    <h1>ResultPage</h1>
             </header>
-            <body className="App-body">
-                <a>FirstContent</a>
-                <Link to="/Home">
-                    <button>Set</button>
-                </Link>
-            </body>
-        </div>
+            <div>
+                <div>
+                    <ol className="List">{listItems}</ol>
+                </div>
+            </div>
+        </>
     );
 }
