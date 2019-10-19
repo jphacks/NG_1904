f7af6749930f810c209ebbebadcf828d3aa3a8cc
maitaken <150441004@ccmailg.meijo-u.ac.jp>
2019-10-19 13:55:07 +0900

    Add : 音声を受け取り形態素解析を行うところまで機能追加

--- App/src/assets/util.js
+++ App/src/assets/util.js
@@ -1,3 +1,11 @@
-export function hello() {
-  console.log('hello')
+import kuromoji from "kuromoji"
+
+export function morphologicalAnalysis(text) {
+  let promise = new Promise((resolve,reject) => {
+    kuromoji.builder({ dicPath: "/dict" }).build((err, tokenizer) => {
+      const tokens = tokenizer.tokenize(text);
+      resolve(tokens);
+    })
+  })
+  return promise
 }
