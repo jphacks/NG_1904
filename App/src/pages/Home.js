e348616e1ae486945a5c39085f74218ffdf1e985
jinyagi <jinjin.0327.jinjin@gmail.com>
2019-10-19 22:42:00 +0900

    Eventの処理をuseEffectに変更

--- App/src/pages/Home.js
+++ App/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react';
+import React,{ useState, useEffect } from 'react';
 import './Home.css';
 import { useHistory, useLocation } from 'react-router-dom';
 
@@ -10,43 +10,50 @@ export default function Home() {
     let [ targetMuzzle,setTargetMuzzle ] = useState({
         'text':'えっ',
     });
-    let [data, setData] = useState({"text":[]});
+    let [data, setData] = useState([]);
+
+    useEffect(() => {
+        if(isRecording){
+            window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+            let recognition = new window.SpeechRecognition();
+            recognition.continuous = true;
+            recognition.lang = "ja-JP";
+
+            recognition.onresult = (event) =>  {
+                let text = event.results[event.results.length-1][0].transcript;
+                let tokens = morphologicalAnalysis(text).then(tokens => {
+                    for(let token of tokens) {
+                        if(token['surface_form'] === targetMuzzle['text']) {
+                            // ここにバイブレーションの動作を追加
+                            vibrate()
+                        }
+                        let data_copy =  Object.create(data);
+                        data_copy.push(token['surface_form']);
+                        //console.log(setData,token['surface_form'])
+                        data_copy.push(token['surface_form']);
+                        //window.confirm(token['surface_form']);
+                        //countedWords.push(token['surface_form']);
+                        setData(data_copy);
+                        console.log(data);
+                    }
+                });
+            }
+            recognition.start();
+            setRecognition(recognition);
+        }
+
+        return function cleanup() {
+            //recognition.abort();
+        };
+    },[isRecording]);
+    
 
     const location = useLocation();    
     
     const history = useHistory();
 
     function recordStart() {
-        window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-        let recognition = new window.SpeechRecognition();
-        recognition.continuous = true;
-        recognition.lang = "ja-JP";
-        setIsRecording(true)
-    
-        // recognition.onerror = function(event){
-        // }
-
-        recognition.onresult = (event) =>  {
-            let text = event.results[event.results.length-1][0].transcript;
-            let tokens = morphologicalAnalysis(text).then(tokens => {
-                for(let token of tokens) {
-                    if(token['surface_form'] === targetMuzzle['text']) {
-                        // ここにバイブレーションの動作を追加
-                        vibrate()
-                    }
-                    // let data_copy =  Object.create(data);
-                    // data_copy.push(token['surface_form']);
-                    //console.log(setData,token['surface_form'])
-                    data["text"].push(token['surface_form']);
-    
-                    //window.confirm(token['surface_form']);
-                    //countedWords.push(token['surface_form']);
-                    console.log(data);
-                }
-            });
-        }
-        recognition.start();
-        setRecognition(recognition);
+        setIsRecording(true);
     }
 
     function recordStop() {
@@ -76,7 +83,7 @@ export default function Home() {
         <body className="App-body">
             <a>HomePage</a>
             {showMuzzleResult}
-            <button onClick={()=>history.push({pathname:'/result',state:{ countedWords: data.text }})}>ToResult</button>
+            <button onClick={()=>history.push({pathname:'/result',state:{ countedWords: data }})}>ToResult</button>
             {recordButton}
         </body>
     );
