0e8f1a6b6a1c41e8f906ad68776ed603398fcf9e
maitaken <150441004@ccmailg.meijo-u.ac.jp>
2019-10-19 23:41:50 +0900

    Add GooAPIの形態素解析に変更

--- App/src/assets/util.js
+++ App/src/assets/util.js
@@ -1,5 +1,7 @@
 import kuromoji from "kuromoji"
 
+const GOO_API_URL = 'https://labs.goo.ne.jp/api/morph'
+
 export function morphologicalAnalysis(text) {
   let promise = new Promise((resolve,reject) => {
     kuromoji.builder({ dicPath: "/dict" }).build((err, tokenizer) => {
@@ -10,6 +12,20 @@ export function morphologicalAnalysis(text) {
   return promise
 }
 
-export async function vibrate() {
+export function vibrate() {
   window.navigator.vibrate([500, 100, 500]);
 } 
+
+export function gooAPIClient(text) {
+  console.log(process.env.REACT_APP_GOO_API_TOKEN)
+  return fetch(GOO_API_URL,{
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify({
+      app_id:process.env.REACT_APP_GOO_API_TOKEN,
+      sentence:text
+    }), 
+  }).then(response => response.json());
+}
