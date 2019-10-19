f9f069b692f1d63f697b958aca629f171bf1462e
jinyagi <jinjin.0327.jinjin@gmail.com>
2019-10-19 14:05:15 +0900

    Routerの動作の書き換え

--- App/src/pages/Home.js
+++ App/src/pages/Home.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import './Home.css';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 export default function Home() {
+    const history = useHistory();
 
     return (
-        <div className="App">
-            <body className="App-body">
-                <a>HomePage</a>
-                <Link to="/result">
-                    <button>ToResult</button>
-                </Link>
-            </body>
-        </div>
+        <body className="App-body">
+            <a>HomePage</a>
+            <button onClick={()=>history.push('/result')}>ToResult</button>
+        </body>
     );
 }
