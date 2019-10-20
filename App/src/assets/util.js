8cef4aaa29a597894ad106224ffaa246dfb3d75c
jinyagi <jinjin.0327.jinjin@gmail.com>
2019-10-20 10:11:59 +0900

    形態素解析のワードを絞る

--- App/src/assets/util.js
+++ App/src/assets/util.js
@@ -1,7 +1,7 @@
 import kuromoji from "kuromoji"
 
 const GOO_API_URL = 'https://labs.goo.ne.jp/api/morph'
-const POS_FILTER = ["名詞"]
+const POS_FILTER = ["名詞|格助詞|引用助詞|連用助詞|終助詞|間投詞|空白|助数詞|助助数詞|冠数詞"]
 
 export function morphologicalAnalysis(text) {
   let promise = new Promise((resolve,reject) => {
