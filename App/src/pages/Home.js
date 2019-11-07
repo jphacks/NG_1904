6cc723f81b8a5614f871ae224d56aa991563844d
jinyagi <jinjin.0327.jinjin@gmail.com>
2019-11-07 13:17:11 +0900

    Fix：解析後に値が存在しない場合のページ遷移を禁止

--- App/src/pages/Home.js
+++ App/src/pages/Home.js
@@ -106,13 +106,18 @@ export default function Home() {
             dispatch(addWords(wc));
             dispatch(addSentences(data));
 
+            /*
             let count = 0;
             for(let j of data){
                 count += (j.match(new RegExp(targetMuzzle, "g")) || []).length ;
             }
             console.log("yagi   :" + count);
-            
-            history.push({pathname:'/result'})
+            */
+           
+            //解析後に値がない場合も遷移しない
+            if(wc.length!==0){
+                history.push({pathname:'/result'})
+            }
         }else{
             dispatch(setPage(PAGES.RECORDS));
         }
