244f7e579177cfb243280bf8e9b0813895080198
yagijin <mukailab@mukailabnoMacBook-Pro.local>
2020-02-09 00:18:14 +0900

    Fix：ページ遷移を合わせる

--- App/src/App.js
+++ App/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { HashRouter, Route, Switch, withRouter } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group'
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
+import First from './pages/First';
 import Home from './pages/Home';
+import Record from './pages/Record';
 import Result from './pages/Result';
 import Select from './pages/Select';
-import Detail from './pages/Detail';
+import Log from './pages/Log';
 import NotFound from './pages/NotFound';
 import './App.scss'
 
@@ -22,11 +25,12 @@ const App = ({ store }) => {
 };
 
 const routes = [
-  { path:"/",Component:Home,transitionClass:"slide" },
+  { path:"/",Component:First,transitionClass:"slide" },
   { path:"/home",Component:Home,transitionClass:"slide" },
-  { path:"/select",Component:Select,transitionClass:"slide" },
+  { path:"/record",Component:Record,transitionClass:"slide" },
   { path:"/result",Component:Result,transitionClass:"slide" },
-  { path:"/detail",Component:Detail,transitionClass:"slide" },
+  { path:"/select",Component:Select,transitionClass:"slide" },
+  { path:"/log",Component:Log,transitionClass:"slide" },
 ]
 
 const Contents = withRouter(({ location }) => {
