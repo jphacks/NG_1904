06995c88da421f26c4ebd68bdd40c536e4abc16b
yagijin <mukailab@mukailabnoMacBook-Pro.local>
2020-02-09 00:11:04 +0900

    Add：ページの作成

---
+++ App/src/pages/Record.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//import { useHistory, useLocation } from 'react-router-dom';
+//import { vibrate, morphologicalAPIClient, wordCount, spawnNotification } from '../common/util';/* morphologicalAnalysis */
+
+import '../App.scss';
+
+//import { useDispatch, useSelector } from 'react-redux';
+//import { setPage, addWords, PAGES, addSentences } from '../actions/actions'
+
+
+export default function Record() {
+    return (
+        <div>Record</div>
+    );
+}
